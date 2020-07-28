ab46edf32a4e9efbf0fc660c79f303591be85c74
Patrick Story <ptedstoryv2@gmail.com>
2020-07-28 18:56:33 -0400

    Create CreatePhoneNumber.cpp

---
+++ cpp/CreatePhoneNumber.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+string createPhoneNumber(const int arr [10]){
+  
+  string number;
+  vector <string> vectorString={"("};
+  
+  for (int i=0; i < 10; i++){
+    string current = to_string(arr[i]);
+    switch(i){
+        case 2:
+          vectorString.push_back(current);
+          vectorString.push_back(")");
+          vectorString.push_back(" ");
+          break;
+        case 5:
+          vectorString.push_back(current);
+          vectorString.push_back("-");
+          break;
+        default:
+          vectorString.push_back(current);
+          break;
+    } 
+  }
+  number = accumulate(begin(vectorString), end(vectorString), number);
+  return number;
+}
