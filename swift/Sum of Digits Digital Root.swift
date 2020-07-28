5343121350b8f38a6e6314a2dd57ff85f184279e
Patrick Story <ptedstoryv2@gmail.com>
2020-07-28 18:54:22 -0400

    Create Sum of Digits Digital Root.swift

---
+++ swift/Sum of Digits Digital Root.swift
@@ -0,0 +1,14 @@
+func digitalRoot(of number: Int) -> Int {
+  print(number)
+  var sum: Int = 0
+  let integerList = String(number).map { Int("\($0)")! }
+  for i in integerList{
+    let integer = Int(i)
+    sum += integer
+    print(i)
+  }
+  while String(sum).count > 1{
+    sum = digitalRoot(of: sum)
+  }
+  return sum;
+}
