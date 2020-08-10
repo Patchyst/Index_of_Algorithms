a45935a912df2a3b9f526868ebe4a65f4ed4f40b
Patrick Story <ptedstoryv2@gmail.com>
2020-08-09 21:09:04 -0400

    Update Digital_Root.swift

--- swift/Digital_Root.swift
+++ swift/Digital_Root.swift
@@ -1,12 +1,10 @@
 // Digital root of a number
 func digitalRoot(of number: Int) -> Int {
-  print(number)
   var sum: Int = 0
   let integerList = String(number).map { Int("\($0)")! }
   for i in integerList{
     let integer = Int(i)
     sum += integer
-    print(i)
   }
   while String(sum).count > 1{
     sum = digitalRoot(of: sum)
