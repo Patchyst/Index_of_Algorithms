b60fbe13c36fdcafedbaa500f93cb40dddcfba34
Patrick Story <ptedstoryv2@gmail.com>
2020-08-11 13:51:50 -0400

    Update SCNVector3-Distance.swift

--- swift/SCNVector3-Distance.swift
+++ swift/SCNVector3-Distance.swift
@@ -1,5 +1,5 @@
 // Function returns the distance between two SCNVector arrays. Helpful for finding the distance between two SCNNodes in ARkit
 func findDistance(vectorOne: SCNVector3, vectorTwo: SCNVector3) -> Float{
-        let distance = sqrt((vectorTwo.x - vectorOne.x)^2 + (vectorTwo.y - vectorOne.y^2) + (vectorTwo.z - vectorOne.z)^2)
-        return distance
-    }
+     let distance = sqrt(pow((vectorTwo.x - vectorOne.x)) + pow((vectorTwo.y - vectorOne.y)) + pow((vectorTwo.z - vectorOne.z)))
+     return distance
+}
