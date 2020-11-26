2420a8b215f32ddd7eeff5d1054ca828029e8fdf
Patrick Story <ptedstoryv2@gmail.com>
2020-11-25 20:22:09 -0500

    Create equalStrings.c

---
+++ C/equalStrings.c
@@ -0,0 +1,11 @@
+bool checkEqual(char charArr[], char charArr2[], bool caseSense){
+    caseSense = false;
+    int lenArray = len(charArr); int lenArray2 = len(charArr2);
+    if(lenArray == lenArray2){
+        for(int i = 0; i<=lenArray; ++i){
+            if(caseSense){if(charArr[i] != charArr2[i]){return false;}}
+            else if(!caseSense){if((char)toupper(charArr[i]) != (char)toupper(charArr2[i])){return false;}}
+                }
+        }
+    return true;
+}
